2d0d8bfd26e750c5e9b9d8c250ca7da8db783e2e
Maciej Jarocki <73995102+macieJJarocki@users.noreply.github.com>
2026-03-17 23:12:29 +0100

    add: basic CRUD  API

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,64 @@
+using System.Net;
+using GameStore.Api.Dtos;
+
+const string GetGameEndpointName = "GatGame";
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+  new (
+    1,
+    "Street Fighter II",
+    "Fighting",
+    19.99M,
+    new DateOnly(1992, 11, 4)
+    ),
+  new (
+    2,
+    "Final Fantasy VII Rebirth",
+    "RPG",
+    69.59M,
+    new DateOnly(1999, 4, 8)
+    ),
+  new (
+    3,
+    "Astro Bot",
+    "Platformer",
+    43.49M,
+    new DateOnly(2000, 7, 23)
+    ),
+];
+
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/1
+
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+
+// POST /games/4
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
+
+// PUT /games/4
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+    games[index] = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
+    return Results.NoContent();
+});
+
+// DELETE /games/4
+app.MapDelete("games/{id}", (int id) =>
+{
+    games.RemoveAll((game) => game.Id == id);
+    return Results.NoContent();
+});
 
 app.Run();
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484e1d3f71b83167b1083bf2c87d4af5350ba10d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
