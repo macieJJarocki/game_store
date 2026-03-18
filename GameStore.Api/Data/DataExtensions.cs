3d2d3ef67fa9d30342eed82645912d83c6d13b9f
Maciej Jarocki <73995102+macieJJarocki@users.noreply.github.com>
2026-03-18 21:21:20 +0100

    update: GameStore.Api

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,58 @@
+using GameStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtensions
+{
+
+    extension(WebApplication app)
+    {
+        // public static void MigrateDb(this WebApplication app)
+        // {
+        //     using var scope = app.Services.CreateScope();
+        //     var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        //     dbContext.Database.Migrate();
+        // }
+        public void MigrateDb()
+        {
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+            dbContext.Database.Migrate();
+        }
+
+
+
+
+    }
+
+    extension(WebApplicationBuilder builder)
+    {
+        public void AddGameStoreDb()
+        {
+            var connectionStr = builder.Configuration.GetConnectionString("GameStore");
+
+            // parameter in UseSeeding
+            builder.Services.AddSqlite<GameStoreContext>(connectionStr,
+            optionsAction: options => options.UseSeeding((context, _) =>
+        {
+            if (!context.Set<Genre>().Any())
+            {
+                context.Set<Genre>().AddRange(
+                    // [
+                    // new Genre { Name = "RPG" },
+                    // new Genre { Name = "Fighting" },
+                    // new Genre { Name = "Racing" }
+                    // ]
+                    //  list is not required?
+                    new Genre { Name = "RPG" },
+                    new Genre { Name = "Fighting" },
+                    new Genre { Name = "Racing" }
+                );
+            }
+            context.SaveChanges();
+        }));
+        }
+    }
+
+}
--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,6 +1,4 @@
-using System;
 using GameStore.Api.Dtos;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace GameStore.Api.Endpoints;
 
